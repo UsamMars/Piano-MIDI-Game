f2017df79b6fef549104dd95f9692daf2fb86504
UsamMars <otherusammars@gmail.com>
2023-10-12 21:03:00 +0500

    Duel Screen Compatability

---
+++ Assets/Scripts/MultidisplayScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultidisplayScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < Display.displays.Length; i++)
+            Display.displays[i].Activate();
+    }
+
+}
